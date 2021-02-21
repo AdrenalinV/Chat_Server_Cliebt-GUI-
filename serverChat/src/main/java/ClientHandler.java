085851e53ad5ae7431dba7a9b351f8843065b09d
AdrenalinV <vadimtsh@gmail.com>
2021-02-21 17:47:00 +0300

    create_Server

---
+++ serverChat/src/main/java/ClientHandler.java
@@ -0,0 +1,127 @@
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class ClientHandler implements Runnable {
+    private final ServerChat server;
+    private final Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private String nickName;
+    private static final long WAIT_TIME_MS = 120000L;
+
+    public ClientHandler(ServerChat server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Object msg;
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+
+            Thread tA = new Thread(() -> {
+                try {
+                    authentication();
+                } catch (IOException | ClassNotFoundException ioException) {
+                }
+            });
+            tA.start();
+            tA.join(WAIT_TIME_MS);
+            if (tA.isAlive()) {
+                sendMsg(TextMessage.of("Service", "Соединение разорвано."));
+                sendMsg(new QuitRequest());
+            }
+            while (true) {
+                msg = in.readObject();
+                if (msg instanceof QuitRequest) {              // отправка при закрытие клиента
+                    out.writeObject(msg);
+                    out.flush();
+                } else if (msg instanceof TextMessage) {      //  сообщения
+                    TextMessage tMSG = (TextMessage) msg;
+                    if (tMSG.getRecipient() != null) {
+                        server.privateSendMessage(tMSG);
+                    } else {
+                        server.broadCastMessage(tMSG);
+                    }
+                }else if (msg instanceof UpdateNickRequest){
+                    UpdateNickRequest uMSG = (UpdateNickRequest) msg;
+                    if(!server.isNickBusy(uMSG.getNewNick())){
+                        server.getAuthService().updateNickName(uMSG.getNewNick(), this.nickName);
+                        this.nickName= uMSG.getNewNick();
+                        server.getUsersList();
+                    }
+                }
+            }
+        } catch (ClassNotFoundException | IOException | InterruptedException ioException) {
+            System.err.println("Handled connection was broken");
+            try {
+                server.removeClient(this);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void authentication() throws IOException, ClassNotFoundException {
+        Object msg = new AuthenticationRequest();
+        out.writeObject(msg);
+        out.flush();
+        while (true) {
+            sendMsg(TextMessage.of("Service", "Введите: Логин Пароль."));
+            msg = in.readObject();
+            if (msg instanceof AuthenticationRequest) {
+                AuthenticationRequest aMSG = (AuthenticationRequest) msg;
+                String nick = server.getAuthService().getNickByLoginPass(aMSG.getLogin(), aMSG.getPass());
+                if (nick != null) {
+                    if (!server.isNickBusy(nick)) {
+                        aMSG.setNick(nick);
+                        aMSG.setStat(true);
+                        out.writeObject(aMSG);
+                        out.flush();
+                        nickName = nick;
+                        server.addClient(this);
+                        return;
+                    } else {
+                        sendMsg(TextMessage.of("Service", "Учетная запись уже используется"));
+                        sendMsg(aMSG);
+
+                    }
+                } else {
+                    if (server.getAuthService().existUser(aMSG.getLogin())) {
+                        sendMsg(TextMessage.of("Service", "Неверный пароль."));
+                    } else {
+                        sendMsg(TextMessage.of("Service", "Пользователь не найден."));
+                        sendMsg(aMSG);
+                        sendMsg(CreateUserRequest.of());
+                    }
+                }
+            } else if (msg instanceof QuitRequest) {
+                sendMsg(msg);
+            } else if (msg instanceof CreateUserRequest) {
+                CreateUserRequest cMSG = (CreateUserRequest) msg;
+                if (cMSG.getLogin() != null && cMSG.getPassword() != null) {
+                    server.getAuthService().createUser(cMSG.getLogin(), cMSG.getPassword());
+                }
+                sendMsg(new AuthenticationRequest());
+            }
+        }
+    }
+
+
+    // отпрвка сообщения текущему пользователю
+    public void sendMsg(Object msg) throws IOException {
+        out.writeObject(msg);
+        out.flush();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+}
+
---
+++ serverChat/src/main/java/ServerChat.java
@@ -0,0 +1,107 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class ServerChat {
+    private static final int DEFAULT_PORT = 8189;
+    private final ConcurrentLinkedDeque<ClientHandler> clients;
+    private final AuthService authService;
+
+    // аутентификация
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    // коннекты и бла бла бла
+    public ServerChat(int port) throws SQLException {
+        authService = new BaseAuthService();
+        authService.start();
+        clients = new ConcurrentLinkedDeque<>();
+        try (ServerSocket server = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("[DEBUG] Client accepted");
+                ClientHandler handler = new ClientHandler(this, socket);
+                new Thread(handler).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Server was broken");
+        } finally {
+            if (authService != null) {
+                authService.stop();
+            }
+        }
+    }
+
+    // проверка свободен ник или нет
+    public boolean isNickBusy(String nick) {
+        for (ClientHandler o : clients) {
+            if (o.getNickName().equals(nick)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int port = -1;
+        if (args != null && args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        if (port == -1) {
+            port = DEFAULT_PORT;
+        }
+        try {
+            new ServerChat(port);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // удаление клиента
+    public void removeClient(ClientHandler clientHandler) throws IOException {
+        clients.remove(clientHandler);
+        System.out.println("[DEBUG] client removed from broadcast server");
+        getUsersList();
+
+    }
+
+    // добавление клиента
+    public void addClient(ClientHandler clientHandler) throws IOException {
+        clients.add(clientHandler);
+        getUsersList();
+    }
+
+    // создание списка активных клиентов
+    public void getUsersList() throws IOException {
+        ArrayList<String> users = new ArrayList<>();
+        for (ClientHandler o : clients) {
+            users.add(o.getNickName());
+        }
+        Collections.sort(users);
+        broadCastMessage(new UserListMSG(users));
+    }
+
+
+    // Общие сообщения
+    public void broadCastMessage(Object msg) throws IOException {
+        for (ClientHandler client : clients) {
+            client.sendMsg(msg);
+        }
+    }
+
+    // Приватные сообщения
+    public void privateSendMessage(TextMessage msg) throws IOException {
+        for (ClientHandler client : clients) {
+            if (client.getNickName().equals(msg.getSender()) || client.getNickName().equals(msg.getRecipient())) {
+                client.sendMsg(msg);
+            }
+        }
+    }
+}
